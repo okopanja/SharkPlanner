54068e41223812fd6d1667304b945b637f2d9618
Oko Panja <okopanja@gmail.com>
2023-02-19 22:57:40 +0100

    Added BS2 and BS3 2011

--- FlightPlanner/CommandGeneratorFactory.lua
+++ FlightPlanner/CommandGeneratorFactory.lua
@@ -1,6 +1,7 @@
 if DEBUG_ENABLED ~= true then
   net.log("Debug mode is disabled")
   require("FlightPlanner.KA50IIICommandGenerator")
+  require("FlightPlanner.KA50IICommandGenerator")
 end
 local CommandGeneratorFactory = {}
 
@@ -10,7 +11,17 @@ function CommandGeneratorFactory.createGenerator(module)
     dofile(lfs.writedir().."Scripts\\FlightPlanner\\KA50IIICommandGenerator.lua")
   end
   if module == 'Ka-50_3' then
-    return KA50IIICommandGenerator:new{}
+    local device = Export.GetDevice(64)
+    if device ~= nil then
+      net.log("Ka-50_3 version 2022 detected")
+      return KA50IIICommandGenerator:new{}
+    else
+      net.log("Ka-50_3 version 2011 detected")
+      return KA50IICommandGenerator:new{}
+    end
+  elseif module == "Ka-50" then
+    net.log("Ka-50 BS2 detected")
+    return KA50IICommandGenerator:new{}
   end
   return nil
 end
---
+++ FlightPlanner/KA50IICommandGenerator.lua
@@ -0,0 +1,48 @@
+require("FlightPlanner.KA50IIICommandGenerator")
+KA50IICommandGenerator = KA50IIICommandGenerator:new()
+
+function KA50IICommandGenerator:_determineNumberOfModePresses()
+  local mode = Export.GetDevice(9):get_mode()
+  mode = tostring(mode.master)..tostring(mode.level_2)..tostring(mode.level_3)..tostring(mode.level_4)
+  if mode == "0000" then
+    return 0
+  elseif self:starts_with(mode,"5") then
+    return 3
+  elseif mode == "5000" then
+    return 3
+  elseif mode == "5500" then
+    return 2
+  elseif mode == "5100" then
+    return 1
+  elseif mode == "5400" then
+    return 4
+  elseif mode == "5310" then
+    return 4
+  elseif mode == "5200" then
+    return 4
+  elseif mode == "5430" then
+    return 4
+  elseif mode == "5240" then
+    return 4
+  end
+  -- net.log("ABRIS Mode: "..mode)
+  return 4
+end
+
+function KA50IICommandGenerator:abrisWorkaroundInitialSNSDrift(commands, selfX, selfZ)
+  net.log("abrisWorkaroundInitialSNSDrift, zoom level: "..self.zoomLevel)
+
+  local dummyRoute = {}
+  dummyRoute[#dummyRoute + 1] = Position:new{x = selfX, y = 0, z = selfZ, longitude = 0, latitude = 0 }
+  net.log("Before abrisUnloadRoute, zoom level: "..self.zoomLevel)
+  self:abrisUnloadRoute(commands)
+  net.log("Before abrisStartRouteEntry, zoom level: "..self.zoomLevel)
+  self:abrisStartRouteEntry(commands)
+  net.log("Before abrisEnterRouteWaypoints, zoom level: "..self.zoomLevel)
+  self:abrisEnterRouteWaypoints(commands, dummyRoute, selfX, selfZ)
+  net.log("Before abrisCompleteRouteEntry, zoom level: "..self.zoomLevel)
+  self:abrisCompleteRouteEntry(commands)
+  for i = 1, 3 do
+    self:abrisPressButton5(commands, "Cycle mode")
+  end
+end
