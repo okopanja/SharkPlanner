e0dfdbfc30c477bc1ca14f54be1997f28ceae047
Oko Panja <okopanja@gmail.com>
2023-07-25 22:14:23 +0200

    Registered skin name

--- SharkPlanner/UI/skins/skin_names.lua
+++ SharkPlanner/UI/skins/skin_names.lua
@@ -1,5 +1,6 @@
 buttonSkinSharkPlannerAmber = 'button_SharkPlanner_amber.skin.lua'
 buttonSkinSharkPlannerGreen = 'button_SharkPlanner_green.skin.lua'
+checkSkinFlipGreen = 'checkbox_flip_green.skin.lua'
 toggleSkinSharkPlannerAmber = 'toggle_SharkPlanner_amber.skin.lua'
 toggleSkinSharkPlannerGreen = 'toggle_SharkPlanner_green.skin.lua'
 toggleSkinSharkPlannerLongAmber = 'toggle_SharkPlanner_long_amber.skin.lua'
