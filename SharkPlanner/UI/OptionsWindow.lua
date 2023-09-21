d5f321d6198e6f792d04f0d74b043d800ca177b1
Oko Panja <okopanja@gmail.com>
2023-09-21 22:34:40 +0200

    Implemented switching from section to section...

--- SharkPlanner/UI/OptionsWindow.lua
+++ SharkPlanner/UI/OptionsWindow.lua
@@ -9,6 +9,10 @@ local SkinUtils = require("SkinUtils")
 local Static = require("Static")
 local Configuration = require("SharkPlanner.Base.Configuration")
 local Button			= require("Button")
+local Panel			= require("Panel")
+local ScrollPane = require("ScrollPane")
+local Static = require("Static")
+local Checkbox = require("CheckBox")
 
 local OptionsWindow = DialogLoader.spawnDialogFromFile(
     lfs.writedir() .. "Scripts\\SharkPlanner\\UI\\OptionsWindow.dlg"
@@ -19,19 +23,55 @@ local skinMappings = {
   ["SA-342 Gazelle"] = SkinHelper.loadSkin("buttonConfigurationSA342")
 }
 
+local staticConfigurationSectionTitleSkin = SkinHelper.loadSkin("staticConfigurationSectionTitle")
+local statusSkin = SkinHelper.loadSkin("staticSkinSharkPlannerStatus")
+
 -- Constructor
 function OptionsWindow:new(o)
     o = o or {}
     setmetatable(o, self)
+    o.sectionPanels = {}
     self.__index = self
     local x, y, w, h = o.crosshairWindow:getBounds()
+    local first = true
     Logging.info("Creating options window")
     for k, v in pairs(Configuration.sections) do
       Logging.debug("Section found: "..k)
       local btn = Button.new()
-      -- btn:setText(v.SectionName)
+      btn.sectionName = k
       btn:setSkin(skinMappings[k])
+      btn:addChangeCallback(
+        function(button)
+          Logging.debug("Clicked on: "..button.sectionName)
+          local currentWidget = o.sectionContent:getWidget(0)
+          local newWidget = o.sectionPanels[button.sectionName]
+          if newWidget ~= currentWidget then
+            Logging.debug("Replacing content")
+            o.sectionContent:removeWidget(currentWidget)
+            o.sectionContent:insertWidget(newWidget)
+          else
+            Logging.debug("Same section selection, ignoring")
+          end
+        end
+      )
+
       o.panelSections:insertWidget(btn, o.panelSections:getWidgetCount() + 1)
+      -- create Panel
+      local panel = Panel.new()
+      panel:setBounds(0,0,200,200)
+      o.sectionPanels[k] = panel
+      local sectionTitle = Static.new()
+      -- o.sectionContent:insertWidget(sectionTitle, o.sectionContent:getWidgetCount() + 1)
+      sectionTitle:setText(k)
+      sectionTitle:setBounds(0,0,200,30)
+      sectionTitle:setVisible(true)
+      sectionTitle:setSkin(staticConfigurationSectionTitleSkin)
+
+      panel:insertWidget(sectionTitle)
+      if first then
+        o.sectionContent:insertWidget(panel, o.sectionContent:getWidgetCount() + 1)
+        first = false
+      end
     end
     -- o.panelSections:getLayout():updateSize()
     o:setBounds(x, y - h, w, h)
---
+++ SharkPlanner/UI/skins/static_configuration_section_title.skin.lua
@@ -0,0 +1,69 @@
+local skin = {
+	["skinData"] = {
+		["params"] = {
+			["textLineHeight"] = 0,
+		},
+		["states"] = {
+			["disabled"] = {
+				[1] = {
+					["text"] = {
+						["bkgColor"] = "0x00000000",
+						["blur"] = 0,
+						["blurColor"] = "0x00000000",
+						["color"] = "0xffffff80",
+						["font"] = "DejaVuLGCSansCondensed.ttf",
+						["fontSize"] = 13,
+						["horzAlign"] = {
+							["offset"] = 0,
+							["type"] = "min",
+						},
+						["lineHeight"] = 0,
+						["lineThroughColor"] = "0x00000000",
+						["lineThroughHeight"] = 1,
+						["shadowColor"] = "0x00000000",
+						["shadowOffset"] = {
+							["horz"] = 0,
+							["vert"] = 0,
+						},
+						["vertAlign"] = {
+							["offset"] = 0,
+							["type"] = "middle",
+						},
+					},
+				},
+			},
+			["released"] = {
+				[1] = {
+					["text"] = {
+						["bkgColor"] = "0x00000000",
+						["blur"] = 0,
+						["blurColor"] = "0x00000000",
+						["color"] = "0xffffffff",
+						["font"] = "DejaVuLGCSansCondensed.ttf",
+						["fontSize"] = 13,
+						["horzAlign"] = {
+							["offset"] = 0,
+							["type"] = "min",
+						},
+						["lineHeight"] = 0,
+						["lineThroughColor"] = "0x00000000",
+						["lineThroughHeight"] = 1,
+						["shadowColor"] = "0x00000000",
+						["shadowOffset"] = {
+							["horz"] = 0,
+							["vert"] = 0,
+						},
+						["vertAlign"] = {
+							["offset"] = 0,
+							["type"] = "middle",
+						},
+					},
+				},
+			},
+		},
+		["type"] = "Static",
+	},
+	["version"] = 1,
+}
+
+return skin
