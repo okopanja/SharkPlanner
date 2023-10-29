833dcddd1fb3a49f1324e347900f01ca3dc86e2c
Oko Panja <okopanja@gmail.com>
2023-10-29 23:01:48 +0100

    Completed the tests

--- SharkPlanner/Mathematics/Geometry.lua
+++ SharkPlanner/Mathematics/Geometry.lua
@@ -1,6 +1,11 @@
 local math = require("math")
 local Arithmetic = require("SharkPlanner.Mathematics.Arithmetic")
 local round_with_precision = Arithmetic.round_with_precision
+local DEBUG = false
+
+local function enable_debug()
+    DEBUG = true
+end
 
 local function round_dms(dms)
     if dms.seconds >=60 then
@@ -21,27 +26,28 @@ local function degAngleToDMSAngle(decimal, degrees_precision, minutes_precision,
         seconds = 0
     }
     degrees_precision = degrees_precision or 0
-    -- minutes_precision = minutes_precision or 0
-    -- seconds_precision = seconds_precision or 0
     local degrees_precision_multiplier = 10 ^ (degrees_precision + 16)
 
     result.sign = decimal < 0 and -1 or 1
 
     local degrees = math.abs(decimal)
     local degrees_rounded = minutes_precision == nil and round_with_precision(degrees, degrees_precision) or math.floor(degrees)
-    -- local degrees_rounded = round(degrees, degrees_precision)
 
     local degrees_fraction = ((degrees * degrees_precision_multiplier) - (degrees_rounded * degrees_precision_multiplier)) / degrees_precision_multiplier
-    print(
-        "degree: "..degrees..", "..
-        "degree_rounded: "..degrees_rounded..", "..
-        "degree_fraction: "..degrees_fraction
-    )
+    if DEBUG then
+        print(
+            "degree: "..degrees..", "..
+            "degree_rounded: "..degrees_rounded..", "..
+            "degree_fraction: "..degrees_fraction
+        )
+    end
     result.degrees = degrees_rounded
+
     if degrees_fraction <= 0 or minutes_precision == nil then
         round_dms(result)
         return result
     end
+
     local minutes_precision_multiplier = 10 ^ (minutes_precision + 16)
 
     -- assert(degrees_fraction >= 0)
@@ -50,34 +56,37 @@ local function degAngleToDMSAngle(decimal, degrees_precision, minutes_precision,
     local minutes_rounded = seconds_precision == nil and round_with_precision(minutes, minutes_precision) or math.floor(minutes)
     -- local minutes_rounded = round(minutes, minute_precision)
     local minutes_fraction = ((minutes * minutes_precision_multiplier) - (minutes_rounded * minutes_precision_multiplier)) / minutes_precision_multiplier
-    print(
-        "minutes: "..minutes..", "..
-        "minutes_rounded: "..minutes_rounded..", "..
-        "minutes_fraction: "..minutes_fraction
-    )
+    if DEBUG then
+        print(
+            "minutes: "..minutes..", "..
+            "minutes_rounded: "..minutes_rounded..", "..
+            "minutes_fraction: "..minutes_fraction
+        )
+    end
     result.minutes = minutes_rounded
     if minutes_fraction <= 0 or seconds_precision == nil then
         round_dms(result)
         return result
     end
-    print("AAAAA")    -- assert(minutes_fraction >= 0)
-
     local seconds_precision_multiplier = 10 ^ (seconds_precision + 16)
 
     local seconds = minutes_fraction * 60
     local seconds_rounded = round_with_precision(seconds, seconds_precision)
     local seconds_fraction = ((seconds * seconds_precision_multiplier) - (seconds_rounded * seconds_precision_multiplier)) / seconds_precision_multiplier
-    print(
-        "seconds: "..seconds..", "..
-        "seconds_rounded: "..seconds_rounded..", "..
-        "seconds_fraction: "..seconds_fraction
-    )
+    if DEBUG then
+        print(
+            "seconds: "..seconds..", "..
+            "seconds_rounded: "..seconds_rounded..", "..
+            "seconds_fraction: "..seconds_fraction
+        )
+    end
     result.seconds = seconds_rounded
     round_dms(result)
     return result
 end
 
 return {
+    enable_debug = enable_debug,
     round_dms = round_dms,
     degAngleToDMSAngle = degAngleToDMSAngle,
 }
--- Tests/run_tests.lua
+++ Tests/run_tests.lua
@@ -6,26 +6,3 @@ for name, test in pairs(geometryTests) do
     test()
     print("Completed: "..name)
 end
--- local latitude = 43.358880080789
--- local longitude = 44.055429646183
--- local Geometry = require("SharkPlanner.Mathematics.Geometry")
--- local inspect = require("SharkPlanner.inspect")
-
--- print("Hello world")
--- local dms = Geometry.decimalAngleToDMS(43.358880080789, 0, 0)
--- assert(dms.degrees == 43)
--- assert(dms.minutes == 21)
--- print(inspect(dms))
--- local dms = Geometry.decimalAngleToDMS(43.358880080789, 0, 1)
--- print(inspect(dms))
--- local dms = Geometry.decimalAngleToDMS(43.5, 0, 0)
--- print(inspect(dms))
-
--- local dms = Geometry.decimalAngleToDMS(3.001, 0, 1)
--- print(inspect(dms))
--- -- local dms = decimalAngleToDMS(3.50, 0, 3)
--- -- print(inspect(dms))
--- -- local dms = decimalAngleToDMS(3.51, 0, 3)
--- -- print(inspect(dms))
--- -- local dms = decimalAngleToDMS(3.51, 0, 1)
--- -- print(inspect(dms))
